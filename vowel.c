578d14a7b6e2cb581427ebbefd9eca5baabd1d57
Archis <archismanpaul09@outlook.com>
2025-02-14 07:04:41 +0530

    advanced vowel checker

--- vowel.c
+++ vowel.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <ctype.h>
 int main()
 {
   char ch;
   char vowels[] = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
   printf("Enter a character: ");
   scanf("%c", &ch);
-  if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+  // if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) //manual checking for alphabet
+  if (isalpha(ch))
   {
     for (int i = 0; i < 10; i++)
     {
