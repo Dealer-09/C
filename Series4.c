3321788e5f49533598ee31ffa3b00b1795a0b8e9
Archis <archismanpaul09@outlook.com>
2025-03-08 19:38:43 +0530

    Update Series4.c

--- Series4.c
+++ Series4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-int main()
-{
-int n;
-double sum=0.0,m;
-printf("enter number upto you want to calculate sum:");
-scanf("%d",&n);
-for(int i=1;i<=n;i++)
-{
-m+=1.0/i;
-sum+=m;
+int main() {
+    int n;
+    double sum = 0.0, m = 0.0;
+    printf("Enter the number up to which you want to calculate the sum: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) 
+    {
+        m = 1.0 / i;
+        sum += m;   
+    }
+    printf("Sum of series is: %lf\n", sum);
 }
-printf("Sum of term is: %lf",sum);
